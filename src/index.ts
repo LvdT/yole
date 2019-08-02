c4797feb3bc86d3cfc3e7706cfb955609b3b41f6
LvdT <lvdt88@gmail.com>
2019-08-02 12:09:24 +0200

    Pinger

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello, World!");
+require('dotenv').config();
+
+import container from "./inversify.config";
+import {TYPES} from "./types";
+import {Bot} from "./bot";
+
+let bot = container.get<Bot>(TYPES.Bot);
+
+bot.listen().then(() => {
+  console.log('Logged in!')
+}).catch((error) => {
+  console.log('Listen failed: ', error)
+});
